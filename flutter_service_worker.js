32541afa61007662147e97895588188624877264
= <=>
2023-05-17 21:33:34 +0700

    initial the website

---
+++ flutter_service_worker.js
@@ -0,0 +1,219 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "c5545cc381e38e648befa44d412f0bbf",
+"assets/assets/icons/airplane.png": "39cbdfaf8a7110693b4915295256e05c",
+"assets/assets/icons/blocked.png": "66eca91800f7960418ac42d91fe23ebb",
+"assets/assets/icons/direct-flight.png": "099983c426882d1c5dbac4bb9f9ffe0b",
+"assets/assets/icons/globe.png": "9df3a2d0314f19cd4e3d50876deab4c6",
+"assets/assets/icons/google.jpg": "d3be0aac5b59064d503df82d85cef889",
+"assets/assets/images/air.png": "e16fd318edf85c89088d24f3dcfba91b",
+"assets/assets/images/airplane-ticket.png": "83ce024c54e81ae43396dcb451518194",
+"assets/assets/images/airport.jpg": "fd7a5a2d574354875e855b7a6a91aabb",
+"assets/assets/images/avatar.jpg": "e96736afe1cd0a014fd88d83c92d0aee",
+"assets/assets/images/bar-chart.png": "c42d4fcfba957e5565ae74bbc21a554a",
+"assets/assets/images/bardcode.png": "81d5e9145fe5cd5c81ca8eb5fc8ce8ae",
+"assets/assets/images/calendar.png": "dba9e53c1d65f4d2ed50ae2e0f7ab8af",
+"assets/assets/images/card.png": "3d35f6e135b8b18d16581da4edc21fa2",
+"assets/assets/images/folders.png": "663d3c09429063dd1c69bd2336c076db",
+"assets/assets/images/password-manager.png": "2587db1f3de33db62dd081d94598fc6e",
+"assets/assets/images/plane-ticket.png": "a7487623a46d9236a6939a7a585df6da",
+"assets/assets/images/plane.jpg": "f314fa2bf6aee360573dc157d87d400f",
+"assets/assets/images/resume.png": "77df3f1dd61603b9fd64370036fe8be5",
+"assets/assets/images/seat.png": "190803d2b955be55f95919f453ce8ba4",
+"assets/assets/images/splash_gif.gif": "f07dff6966ac48982773fbb149bfde48",
+"assets/assets/images/vecteezy_airplane.jpg": "8f645436b31f4a4da2aee7a72bf44f4e",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/NOTICES": "b56c9d3cbec264722e402ad500d7075f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "dd728e7117c99129254ebabed0423bcc",
+"/": "dd728e7117c99129254ebabed0423bcc",
+"main.dart.js": "522d55a375aeb150bdd89499c5690204",
+"main.dart.js_1.part.js": "76c575b993b6856d338b9bde4307ebf8",
+"manifest.json": "31ccd05bf1a41a98b6c9aa4b0e109527",
+"version.json": "b038e7fca0b462e8db715bcccbc7a498"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ main.dart.js_1.part.js
@@ -0,0 +1,42 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+bow(d){return"address "+B.h(d)},
+box(d){return"id "+B.h(d)},
+boy(d){return"name "+B.h(d)},
+boz(d){return B.h(d)+" Customer"},
+boA(d){return B.h(d)+" Employee"},
+boB(d){return"We have sent a verification email to: "+B.h(d)},
+bbn(d){return B.aK(["Minutes",A.ad("Minutes"),"accountSetting",A.ad("Account Settings"),"actions",A.ad("Actions"),"activities",A.ad("Activities"),"addNewAirport",A.ad("Add New Airport"),"addNewFlight",A.ad("Add New Flight"),"addNewWidgets",A.ad("Add new widgets"),"addPlan",A.ad("Add Plan"),"address",A.ad("Address"),"addressData",A.byu(),"age",A.ad("Age"),"airport",A.ad("Airport"),"airportFinish",A.ad("Airport Finish"),"airportOverview",A.ad("Airport Overview"),"airportStart",A.ad("Airport Start"),"allBooking",A.ad("All Booking"),"amount",A.ad("Amount"),"applyNewPassword",A.ad("Apply New Password"),"birthday",A.ad("Birthday"),"blockTime",A.ad("Block Time"),"bookingTime",A.ad("Booking Time"),"bookingType",A.ad("Booking type"),"boy",A.ad("Boy"),"business",A.ad("Business"),"businessClass",A.ad("Business Class"),"businessClassTicket",A.ad("Business Class Ticket"),"cabinCrew",A.ad("Cabin crew"),"chair",A.ad("Chair"),"changePassword",A.ad("Change Password"),"class1",A.ad("Class"),"clickToUpload",A.ad("Click to upload"),"commingFlights",A.ad("Comming Flights"),"country",A.ad("Country"),"createYourNewPassword",A.ad("Create Your New Password"),"currentPassword",A.ad("Current Password"),"customer",A.ad("Customer"),"customerChartView",A.ad("Customer Chart view"),"customerInformation",A.ad("Customer Information"),"darkMode",A.ad("Dark Mode"),"dashboard",A.ad("Dashboard"),"date",A.ad("Date"),"dateBorn",A.ad("Date Born"),"dateFinish",A.ad("Date Finish"),"dateRange",A.ad("Date range"),"dateStart",A.ad("Date Start"),"deadlineForTicketCancellation",A.ad("Deadline For Ticket Cancellation"),"deadlineForTicketPurchase",A.ad("Deadline For Ticket Purchase"),"delete",A.ad("Delete"),"departureDate",A.ad("Departure Date"),"destination",A.ad("Destination"),"direct",A.ad("Direct"),"directFlight",A.ad("Direct Flight "),"donHaveAnAccount",A.ad("Don't have an account ?"),"driver",A.ad("Driver"),"duration",A.ad("Duration"),"economyClass",A.ad("Economy Class"),"economyClassTicket",A.ad("Economy Class Ticket"),"edit",A.ad("Edit"),"editAirport",A.ad("Edit Airport"),"editFlight",A.ad("Edit Flight"),"email",A.ad("Email"),"emailAddress",A.ad("Email Address"),"emailPhoneDetail",A.ad("Email and Phone Details"),"employee",A.ad("Employee"),"enterCurrentPassword",A.ad("Enter your current Password"),"enterNewPassword",A.ad("Enter your new Password"),"enterYourEmail",A.ad("Enter your email"),"enterYourNewPassword",A.ad("Enter Your New Password"),"enterYourPassword",A.ad("Enter your Password"),"exp",A.ad("Exp"),"export",A.ad("Export"),"female",A.ad("Female"),"filter",A.ad("Filter"),"firstClass",A.ad("First Class"),"firstClassTicket",A.ad("First Class Ticket"),"firstOfficer",A.ad("First Officer"),"flight",A.ad("Flight"),"flightAdmin",A.ad("Flight Admin"),"flightAttendant",A.ad("Flight attendant"),"flightChartView",A.ad("Flight Chart view"),"flightDetail",A.ad("Flight Detail"),"flightInformation",A.ad("Flight Information"),"flightTickets",A.ad("Flight tickets"),"flightTime",A.ad("Flight Time"),"forgotPassword",A.ad("Forgot Password"),"gate",A.ad("Gate"),"gender",A.ad("Gender"),"generalSetting",A.ad("General Settings"),"hideInformation",A.ad("Hide Information"),"hours",A.ad("Hours"),"id",A.ad("ID"),"idData",A.byv(),"identityNum",A.ad("Identity Number"),"image",A.ad("Image"),"lightMode",A.ad("LightMode"),"listCustomer",A.ad("List Customer"),"listFlights",A.ad("List Flights"),"listTicket",A.ad("List tickets"),"location",A.ad("Location"),"logIn",A.ad("Log In"),"login",A.ad("Login"),"loginWithGoogle",A.ad("Login with Google"),"luggage",A.ad("luggage"),"male",A.ad("Male"),"maximumBlockTime",A.ad("Maximum Block Time"),"maximumFlightTime",A.ad("Maximum Flight Time"),"maximumNumberOfLayovers",A.ad("Maximum Number of Layovers"),"minimumBlockTime",A.ad("Minimum Block Time"),"minimumFlightTime",A.ad("Minimum Flight Time"),"month",A.ad("Month"),"more",A.ad("More"),"name",A.ad("Name"),"nameData",A.byw(),"newPassword",A.ad("New Password"),"noCustomer",A.ad("No Customer"),"numberCustomer",A.byx(),"numberEmployee",A.byy(),"numberOfAirport",A.ad("Number Of Airports"),"or",A.ad("Or"),"orDrag",A.ad(" or drag and drop SVG, PNG, JPG or GIF (max 800x400px)"),"overview",A.ad("Overview"),"passengers",A.ad("Passengers"),"password",A.ad("Password"),"passwordSettings",A.ad("Password Settings"),"payment",A.ad("Payment"),"people",A.ad("People"),"personalInfo",A.ad("Personal Info"),"phoneNumber",A.ad("Phone Number"),"pickImage",A.ad("Pick Image"),"pilot",A.ad("Pilot"),"planeTicket",A.ad("Plane Ticket"),"pleaseContact",A.ad("Please ! Contact Administrator to be provided Account!"),"pleaseEnterEmailToFindAccount",A.ad("Please Enter your email to find your account"),"pleaseLogin",A.ad("Please login to use our service"),"premiumEconomyClass",A.ad("Premium Economy Class"),"premiumEconomyClassTicket",A.ad("Premium Economy Class Ticket"),"price",A.ad("Price"),"principleSetting",A.ad("Principle Setting"),"reenterPassword",A.ad("Reenter Password"),"reenterYourPassword",A.ad("Reenter Your Password"),"rememberInformation",A.ad("Remember Information"),"retypePassword",A.ad("Enter your retype password"),"ruleSettings",A.ad("Rule Settings"),"sale",A.ad("Sale"),"salePerformance",A.ad("Sale Performance"),"saveChange",A.ad("Save Change"),"searchAnything",A.ad("Search Anything..."),"seat",A.ad("Seat"),"sentVerificationEmail",A.ad("Sent Verification Email"),"setting",A.ad("Setting"),"showMoreInformation",A.ad("Show more Information"),"startFrom",A.ad("Start From"),"theServiceHaveNotFinished",A.ad("The Service have not finished yet !!!"),"thisWillDisplay",A.ad("This will be display on your profile"),"ticketNumber",A.ad("Number of Ticket"),"times",A.ad("Times"),"to",A.ad("To"),"totalAirport",A.ad("Total Airport"),"totalCustomer",A.ad("Total Customer"),"totalFlight",A.ad("Total Flight"),"totalPayment",A.ad("Total Payment"),"update",A.ad("Update"),"updateProfile",A.ad("Update Profile"),"updateYourAvatar",A.ad("Update your avatar and personal information here"),"uploadProfileImage",A.ad("Upload Profile Image"),"username",A.ad("Username"),"view",A.ad("View"),"viewDetail",A.ad("View Detail"),"visited10Of100Countries",A.ad("Visited 10 of 100 countries"),"warning",A.ad("Warning"),"weHaveSentAVerification",A.byz(),"welcomeoFlightAdmin",A.ad("Welcome to Flight Admin"),"yourName",A.ad("Your Name"),"yourPhoto",A.ad("Your Photo")],y.g,y.a)},
+a3o:function a3o(d){this.a=d},
+ad(d){return new A.aA1(d)},
+wR:function wR(){},
+aA1:function aA1(d){this.a=d},
+bwB(d,e,f){if(d!=="")return d
+return e}},B
+A=a.updateHolder(c[3],A)
+B=c[0]
+A.a3o.prototype={}
+A.wR.prototype={
+aCS(d,e,f,g,h,i){var x=this.a.i(0,A.bwB(f,d,h))
+if(x==null)return d
+else return B.baE(x,g)},
+i(d,e){return this.a.i(0,e)},
+k(d){return"en"}}
+var z=a.updateTypes(["f(@)","b_<f,kY>(@)"])
+A.aA1.prototype={
+$0(){return this.a},
+$S:64};(function installTearOffs(){var x=a._static_1
+x(A,"byu","bow",0)
+x(A,"byv","box",0)
+x(A,"byw","boy",0)
+x(A,"byx","boz",0)
+x(A,"byy","boA",0)
+x(A,"byz","boB",0)
+x(A,"byt","bbn",1)})();(function inheritance(){var x=a.inherit
+x(A.wR,B.G)
+x(A.a3o,A.wR)
+x(A.aA1,B.GC)})()
+B.bee(b.typeUniverse,JSON.parse('{"a3o":{"wR":[]}}'))
+var y={a:B.a1("kY"),g:B.a1("f")};(function lazyInitializers(){var x=a.lazyFinal
+x($,"bFf","bjK",()=>new A.a3o(A.bbn(A.byt())))})()}
+$__dart_deferred_initializers__["JZfSMHFglnMywss6+SP03aB4NIk="] = $__dart_deferred_initializers__.current
